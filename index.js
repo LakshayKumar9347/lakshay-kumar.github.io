1c009f663d49922342bfaf57df5b221c2798a966
Lakshay Kumar Kashyap <131148878+LakshayKumar9347@users.noreply.github.com>
2023-05-13 14:03:33 +0530

    intitial commint before update
    update is on the way of enhancing portfolio sectoin

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const navbarScroll = document.querySelectorAll('.nav-link-scroll')
+const navbarScroll1 = document.querySelectorAll('.nav-link-scroll1')
 
-navbarScroll.forEach((item)=>{
+navbarScroll1.forEach((item)=>{
   item.addEventListener('click',()=>{
     let el = document.getElementById('scroll1') 
     el.scrollIntoView({behavior:"smooth",block:"start"}) 
   })
 })
-navbarScroll.forEach((item)=>{
+const navbarScroll2 = document.querySelectorAll('.nav-link-scroll2')
+
+navbarScroll2.forEach((item)=>{
   item.addEventListener('click',()=>{
     let el = document.getElementById('scroll2') 
     el.scrollIntoView({behavior:"smooth"}) 
